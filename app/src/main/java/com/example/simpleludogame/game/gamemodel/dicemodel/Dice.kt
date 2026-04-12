049146fd3bb6ba19e3f754fbab9117c1c69fcf09
ashwanikd0123 <ashwanikd0123@gmail.com>
2026-04-12 20:38:54 +0530

    no straight 4 sixes

--- app/src/main/java/com/example/simpleludogame/game/gamemodel/dicemodel/Dice.kt
+++ app/src/main/java/com/example/simpleludogame/game/gamemodel/dicemodel/Dice.kt
@@ -2,8 +2,21 @@ package com.example.simpleludogame.game.gamemodel.dicemodel
 
 class Dice {
     companion object {
+        var sixCount = 0
         fun getRandomNumber(): Int {
-            return (Math.random() * 6.0 + 1.0).toInt()
+            val value = (Math.random() * 6.0 + 1.0).toInt()
+            if (value != 6) {
+                sixCount = 0
+                return value
+            }
+
+            sixCount++
+            if (sixCount == 4) {
+                sixCount = 0
+                return  (Math.random() * 5.0 + 1.0).toInt()
+            }
+
+            return value
         }
     }
 }
